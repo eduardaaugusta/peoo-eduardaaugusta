4c55196050382f0c8cb3a65c821d2a937fca6c8b
Eduarda Augusta <eduarda.augusta@escolar.ifrn.edu.br>
2023-05-25 11:20:31 -0300

    4medpond.py
    media ponderada da notas

---
+++ 4medpond.py
@@ -0,0 +1,17 @@
+print('Digite as 5 notas:')
+n1 = float(input())
+n2 = float(input())
+n3 = float(input())
+n4 = float(input())
+n5 = float(input())
+
+calc1 = n1 //1 
+print('A media ponderada do 1°BIM =',calc1)
+calc2 = n2 //2
+print('A media pondetada do 2°BIM =',calc2)
+calc3 = n3 //3
+print('A media ponderada do 3°BIM =',calc3)
+calc4 = n4 //4
+print('A media ponderada do 4°BIM =',calc4)
+calc5= n5 //5
+print('A media ponderada do 5°BIM =',calc5)
