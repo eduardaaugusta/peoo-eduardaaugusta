6478def5b26227604e15f8f54809f9b5c0e7005f
Eduarda Augusta <122638796+eduardaaugusta@users.noreply.github.com>
2023-06-01 09:38:28 -0300

    Delete 18operamat.py

--- 18operamat.py
+++
@@ -1,8 +0,0 @@
-print('Digite 2 números inteiros:')
-n1 = int(input())
-n2 = int(input())
-soma = n1 + n2
-sub = n1 - n2
-multi = n1 * n2
-div = n1 / n2
-print('O resultado das quatro operações: SOMA = {}, SUBTRAÇÃO = {}, MULTIPLICAÇÃO = {}, DIVISÃO = {}'.format(soma, sub, multi, div))
