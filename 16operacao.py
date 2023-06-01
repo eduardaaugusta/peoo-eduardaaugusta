d0c3a0de34c7aba6d1f360a7872b443adebc5f42
Eduarda Augusta <122638796+eduardaaugusta@users.noreply.github.com>
2023-06-01 09:38:04 -0300

    Delete 16operacao.py

--- 16operacao.py
+++
@@ -1,8 +0,0 @@
-print('Digite um numero inteiro:')
-n1 = int(input())
-print('Digite outro numero inteiro:')
-n2 = int(input())
-soma = n1 + n2
-sub = n1 - n2
-multi = n1 * n2
-print('Soma = {}, Subtração = {}, Multiplicação = {}'.format(soma, sub, multi))
