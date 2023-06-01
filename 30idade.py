084a599b73847637db1d115c2753b3678a7a3546
Eduarda Augusta <122638796+eduardaaugusta@users.noreply.github.com>
2023-06-01 09:40:53 -0300

    Delete 30idade.py

--- 30idade.py
+++
@@ -1,7 +0,0 @@
-print('Digite seu ano de nascimento:')
-ano = int(input())
-print('Digite o ano atual')
-anoatual = int(input())
-calc = anoatual - ano
-idade50 = 2050 - ano
-print('Sua idade atual é {} e sua idade em 2050 é {}'.format(calc, idade50))
