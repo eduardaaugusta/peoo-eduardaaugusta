f9af4cdcfe05c4cca023958270c6f7c254486fd4
Eduarda Augusta <122638796+eduardaaugusta@users.noreply.github.com>
2023-06-01 09:45:47 -0300

    1equa.py

---
+++ sequencial/1equa.py
@@ -0,0 +1,9 @@
+print('Digite o valor de A:')
+A = int(input())
+print('Digite o valor de B:')
+B = float(input())
+print('Digite o valor de C:')
+C = int(input())
+
+calc = A**2 + B + C
+print('O resultado da equação de 2° grau =',calc)
